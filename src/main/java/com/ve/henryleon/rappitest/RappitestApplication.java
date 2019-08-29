2c9c95c53ff318409911f35e52103b0d03b4e516
Henry Leon Gomez <helg18@gmail.com>
2019-08-29 13:39:52 -0400

    Initial Commit

---
+++ src/main/java/com/ve/henryleon/rappitest/RappitestApplication.java
@@ -0,0 +1,13 @@
+package com.ve.henryleon.rappitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RappitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RappitestApplication.class, args);
+	}
+
+}
